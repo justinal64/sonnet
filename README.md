665c2f6615b3397f1bee9126bcfb81f524bda8e5
justin leggett <justin@codingrookie.com>
2016-08-23 14:29:30 -0500

    cleaned uo the readme and added a favicon

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-<h3>In this project we use basic js operators to get the value of a div and replace some words before adding the div back.</h3>
+#Sonnet
+
+
+
+## Description
+In this exercise I get the value of the div and manipulate the data using Javascript.
+
+## How to run
+In your terminal run:
+```
+http-server -p 9999
+```
+then navigate to http://localhost:9999 in your browser.
+
+## Contributors
+- [Justin Leggett](https://github.com/justinal64)
--- sonnet.js
+++ sonnet.js
@@ -1,29 +1,54 @@
 // Task 1
 // Take the contents of the sonnet div and place it in a variable
 var divContent = document.getElementById("sonnet").innerHTML;
-// console.log(divContent);
-
-// Task 2
-// Find and output the starting position of the word "orphans"
-console.log(divContent.indexOf("orphans"));
-
-// Task 3
-// Output the number of characters in the sonnet
-console.log(divContent.length);
 
 // Task 4
 // Replace the first occurance of the string "winter" with "yuletide"
-var newContent = (divContent.replace(/winter/g, "yuletide"));
-// console.log(newContent);
+var newContent = divContent.replace(/winter/, "yuletide");
 
 // Task 5
 // Replace all occurances of the string "the" with "a large"
-newContent = newContent.replace(/the/g, "large");
-// console.log(newContent);
+newContent = newContent.replace(/the /gi, " a large "); // gi replaced The or the or tHe etc etc
 
 // Task 6
 // Set the content of the sonnet div with the new string
 document.getElementById("sonnet").innerHTML = newContent;
 
-// Original Text
-console.log(divContent);
+// How to use a switch statement
+switch(divContent.indexOf("orphans")) {
+    case 441:
+        console.log("Case = 441");
+        break;
+    case 442:
+        console.log("Case = 442");
+        break;
+    case 443:
+        console.log("Case = 443");
+        break;
+    default:
+        console.log("This is the default answer")
+}
+
+// Another way to use a switch statement
+switch(true) {
+    case divContent.indexOf("orphans") > 410:
+        console.log("Case = 441");
+        break;
+    case divContent.indexOf("orphans") <= 410:
+        console.log("Case = 442");
+        break;
+    case 443:
+        console.log("Case = 443");
+        break;
+    default:
+        console.log("This is the default answer")
+}
+
+
+
+
+
+
+
+
+
